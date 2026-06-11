2ee11ac794f1eb4ea6c49c212c7000983d872659
Luis <lp1172@yahoo.com>
2026-06-11 13:59:06 -0500

    adding to do functions

--- app.py
+++ app.py
@@ -7,8 +7,41 @@ def init_db():
     conn = sqlite3.connect("todo.db")
     cursor = conn.cursor()
     cursor.execute("""
-        CREATE TABLE IF NOT EXISTS tasks (
+        CREATE TABLE IF NOT EXISTS todo (
             id INTEGER PRIMART KEY AUTOINCREMENT,
             title TEXT NOT NULL,
             completed INTEGER DEFAULT 0)
     """)
+    conn.commit()
+    conn.close()
+
+init_db()
+
+@app.route("/")
+def index():
+    conn = sqlite3.connect("todo.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT id, title, completed FROM tasks")
+    tasks = cursor.fetchall()
+    conn.close()
+    return render_template("index.html", tasks=tasks)
+
+@app.route("/add", methods=["POST"])
+def add_task():
+    title = request.form.get("title")
+    if title:
+        conn = sqlite3.connect("todo.db")
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO tasks (title) VALUES (?)", (title,))
+        conn.commit()
+        conn.close()
+        return redirect("/")
+
+@app.route("/complete/<int:task_id>")
+def complete_task(task_id):
+
+@app.route("/delete/<int:task_id>")
+def delete_task(task_id):
+
+if __name__ == "__main__":
+    app.run(debug=True)
